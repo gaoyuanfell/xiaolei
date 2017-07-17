e8502f11368188840bd4216976c5b561986230a2
moka <gaoyuanfell@sina.com>
2017-07-17 14:23:37 +0800

    1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,25 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {TranslateService} from "@ngx-translate/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-    constructor(private translate: TranslateService){
+    constructor(private translate: TranslateService) {
         translate.setDefaultLang('en');
         translate.use('en');
     }
 
     private isEn = true;
-    change(){
-        if(this.isEn){
+
+    change() {
+        if (this.isEn) {
             this.translate.setDefaultLang('zh');
             this.translate.use('zh');
             this.isEn = false;
-        }else{
+        } else {
             this.translate.setDefaultLang('en');
             this.translate.use('en');
             this.isEn = true;
